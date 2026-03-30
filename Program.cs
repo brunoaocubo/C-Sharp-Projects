0cf43cccef1be445ac91f14c436c883f19a93361
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-03-30 17:19:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace projects
+{
+    public class  Program()
+    {
+        static void AddToList(List<object> list, string value)
+        {
+            list.Add(value);
+
+            foreach (object obj in list)
+            {
+                if(list.Count >= 5)
+                {
+                    Console.WriteLine(obj);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<object> listObj = new List<object>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Insira o valor do {i}º número: ");
+                var myInput = Console.ReadLine();
+
+                if (myInput != null) { AddToList(listObj, myInput); }
+            }
+        }
+    }
+}
